7930200114bb755d3dcaf510d449958c65261757
RahimTSNE <rahimtsne@gmail.com>
2018-10-17 21:23:54 +0530

    Create experimental_htcoijs.js

---
+++ 00002/experimental_htcoijs.js
@@ -0,0 +1,19 @@
+// how to create objects in javascript
+
+let Person = {};
+
+Person = {
+    properties : {
+    firstName : "rah",
+    middleName : "md",
+    lastName : "an",
+    age : 33,
+    haircolor : "black"
+    },
+    methods : {
+    greet : function(){
+                console.log("ny name is " + firstName);
+                }
+    }
+}
+    
